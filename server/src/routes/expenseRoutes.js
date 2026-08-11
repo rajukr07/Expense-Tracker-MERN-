1fc6b947733af06d0429c241059884e68edbed85
Raju Kumar <rajukumarsdr95@gmail.com>
2026-08-11 19:22:29 +0530

    complete protected expense CRUD API

--- server/src/routes/expenseRoutes.js
+++ server/src/routes/expenseRoutes.js
@@ -2,12 +2,25 @@ const express = require("express");
 
 const {
   createExpense,
+  getExpenses,
+  getExpenseById,
+  updateExpense,
+  deleteExpense,
 } = require("../controllers/expenseController");
 
 const protect = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
-router.post("/", protect, createExpense);
+router
+  .route("/")
+  .post(protect, createExpense)
+  .get(protect, getExpenses);
+
+router
+  .route("/:id")
+  .get(protect, getExpenseById)
+  .put(protect, updateExpense)
+  .delete(protect, deleteExpense);
 
 module.exports = router;
